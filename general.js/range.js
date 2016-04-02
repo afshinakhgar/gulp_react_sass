9ba7b1058a0d70e79ad1a6e09717d4f7be508d4b
Afshin Akhgar <Afshin Akhgar>
2016-04-03 00:10:05 +0430

    price range

--- general.js/range.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-	$('#range').on("change", function() {
-		var price = this.value.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")
-
-
-		$('.output').val(price +"  ریال" );
-	}).trigger("change");
-});
